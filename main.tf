05605c335e677c269ec861e429afc2364e583367
Pratik Bhongale <155747247+pratikbhongale@users.noreply.github.com>
2025-08-13 15:36:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource local_file my_file {
+	filename = "automate.txt"
+	content = "I have started to work on terraform"
+	
+}
---
+++ s3.tf
@@ -0,0 +1,5 @@
+# This is S3 Bucket
+resource aws_s3_bucket my_bucket {
+	bucket = "terraform-demo-pratik"
+}
+
